465bff30970ab295b70bae0406e60754422f35ef
Duong Tran <duongtran845@gmail.com>
2021-06-18 14:23:47 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
+import sqlite3 as lite
+import os
 
 def get(page):
     response = requests.get("https://agirlmagazine.tumblr.com/page/{0}".format(page))
@@ -11,13 +12,20 @@ def get(page):
     return links
 
 if __name__ == "__main__":
+    con = None
+    path = os.path.dirname(__file__) + "\\test.db"
+    con = lite.connect(path)
+    page = os.getenv("PAGE", "1")
     data = {}
     data['girl'] = []
     id = 1
-    for x in range(1):
+    links = []
+    for x in range(int(page)):
         link = get(x+1)
-        for l in link:
-            data['girl'].append({"id": id, 'url': l})
-            id+1;
+        links.extend(link)
+    for l in links:
+        data['girl'].append({"id": id, 'url': l})
+        id+=1;
+
     with open('./data/data.json', 'w') as outfile:
         json.dump(data, outfile)
